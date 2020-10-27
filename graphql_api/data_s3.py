7e99271797f3f7a16745bd4915f5f3afe83d2d51
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-10-27 23:56:32 +1300

    improved data model

--- graphql_api/data_s3.py
+++ graphql_api/data_s3.py
@@ -2,34 +2,41 @@ import boto3
 import os
 import json
 from io import BytesIO
+import datetime as dt
+# from graphql import GraphQLError
 
 class TaskResultData():
-
+  
     def __init__(self):
         self.client = boto3.client('s3',
                           aws_access_key_id='S3RVER', 
                           aws_secret_access_key='S3RVER',
                           endpoint_url='http://localhost:4569')
-        self.bucket_name = os.environ['S3_BUCKET_NAME']
-
+        self.bucket_name = os.environ.get('S3_BUCKET_NAME', "UNDEFINED")
         self.s3 = boto3.resource('s3')
         self.bucket = self.s3.Bucket(self.bucket_name, client=self.client)
-        self.prefix="TaskResult"
-
+        self.prefix = self.__class__.__name__
+        
     def get_next_id(self):
         size = sum(1 for _ in self.bucket.objects.filter(Prefix='%s/' % self.prefix))
         return size
 
-    def create(self, task_result_name, faction_id=''):
-        from .schema import TaskResult
+    def create(self, **kwargs):
+        from .schema import OpenshaRuptureGenResult
         next_id  = str(self.get_next_id())
-        new = TaskResult(id=next_id, name=task_result_name)
+        if not  kwargs['started'].tzname(): #must have a timezone set
+            raise ValueError("'started' DateTime() field must have a timezone set.")
+        
+        #kwargs['started'] = kwargs['started'].isoformat()
+        new = OpenshaRuptureGenResult(next_id, **kwargs)
+        body = new.__dict__.copy()
+        body['started'] = body['started'].isoformat()
         key = "%s/%s/%s" % (self.prefix, next_id, "object.json")
-        response = self.bucket.put_object(Key=key, Body=json.dumps(new.__dict__))
+        response = self.bucket.put_object(Key=key, Body=json.dumps(body))
         return new
 
     def get_one(self, task_result_id):
-        from .schema import TaskResult
+        from .schema import OpenshaRuptureGenResult, TaskResultType
         key = "%s/%s/%s" % (self.prefix, task_result_id, "object.json")
         obj = self.s3.Object(bucket_name=self.bucket_name,
                         key=key,
@@ -38,7 +45,13 @@ class TaskResultData():
         obj.download_fileobj(fo)
         fo.seek(0)
         jsondata = json.load(fo)
-        return TaskResult(**jsondata)
+        
+        #Field type transforms...
+        started = jsondata.get('started')
+        if started:
+            jsondata['started'] = dt.datetime.fromisoformat(started)
+        print("get_one", jsondata)            
+        return OpenshaRuptureGenResult(**jsondata)
 
     def get_all(self):
         task_results = []
--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,58 +1,75 @@
 import graphene
 from graphene import relay
 from graphene_file_upload.scalars import Upload
+from graphene import Enum
 
 from .data_s3 import TaskResultData, get_faction
 
-class TaskResult(graphene.ObjectType):
-    """A TaskResult in the Star Wars saga"""
+class TaskResultType(Enum):
+    TEST_RESULT = "test_result"
+    JOB_RESULT = "job_result"
 
+class RupturePermutationStrategy(Enum):
+    UCERF3 = 'ucerf3'
+    DOWNDIP = 'downdip'
+    POINTS = 'points'
+    
+class RuptureGeneratorArgs():
+    max_jump_distance = graphene.Float()
+    max_sub_section_length = graphene.Float()
+    min_sub_sections_per_parent = graphene.Int()
+    max_cumulative_azimuth = graphene.Float()
+    permutation_strategy = RupturePermutationStrategy()
+    
+class RuptureGeneratorArgsInput(RuptureGeneratorArgs, graphene.InputObjectType):
+    pass
+
+class RuptureGeneratorArgsOutput(RuptureGeneratorArgs, graphene.ObjectType):  
+    pass      
+
+class TaskResult(graphene.Interface):
+    """A TaskResult in the NSHM saga"""
     class Meta:
-        interfaces = (relay.Node,)
-
-    name = graphene.String(description="The name of the TaskResult.")
+        interfaces = (relay.Node, ) 
+        
+    name = graphene.String() # deprecated
+    tasktype = graphene.Field(TaskResultType) #deprecated
+    started = graphene.DateTime(description="The time the task was started")
+    duration = graphene.Float(description="the final duraton of the task in seconds")  
+                              
+class OpenshaRuptureGenResult(graphene.ObjectType):
+    """An OpenshaRuptureGenResult in the NSHM process"""
+    class Meta:
+        interfaces = (relay.Node, TaskResult)
+ 
+    rupture_generator_args = graphene.Field(RuptureGeneratorArgsOutput)
 
     @classmethod
-    def get_node(cls, info, id):
-        return db_root.get_one(id)
-
-class TaskResultConnection(relay.Connection):
+    def get_node(cls, info, _id):
+        node =  db_root.get_one(_id)
+        #print('NODE', node, node.id, node.type )
+        return node   
+    
+class OpenshaRuptureGenResultConnection(relay.Connection):
     class Meta:
-        node = TaskResult
-
-class Faction(graphene.ObjectType):
-    """A faction in the Star Wars saga"""
-
-    class Meta:
-        interfaces = (relay.Node,)
-
-    name = graphene.String(description="The name of the faction.")
-    task_results = relay.ConnectionField(
-        TaskResultConnection, description="The TaskResults used by the faction."
-    )
-
-    def resolve_task_results(self, info, **args):
-        # Transform the instance task_result_ids into real instances
-        return [db_root.get_one(task_result_id) for task_result_id in self.task_results]
-
-    @classmethod
-    def get_node(cls, info, id):
-        return get_faction(id)
-
-
-class CreateTaskResult(relay.ClientIDMutation):
+        node = OpenshaRuptureGenResult
+      
+class CreateOpenshaRuptureGenResult(relay.ClientIDMutation):
     class Input:
-        task_result_name = graphene.String(required=True)
-        faction_id = graphene.String(required=True)
-
-    task_result = graphene.Field(TaskResult)
-    faction = graphene.Field(Faction)
+        name = graphene.String() # deprecated
+        tasktype = graphene.Field(TaskResultType) #deprecated        
+        started = graphene.DateTime(description="The time the task was started")
+        duration = graphene.Float(description="the final duraton of the task in seconds")
+        rupture_generator_args = RuptureGeneratorArgsInput()
+        
+    task_result = graphene.Field(OpenshaRuptureGenResult)
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, task_result_name, faction_id, client_mutation_id=None):
-        task_result = db_root.create(task_result_name, faction_id)
-        faction = get_faction(faction_id)
-        return CreateTaskResult(task_result=task_result, faction=faction)
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        print("mutate_and_get_payload: ", kwargs)
+        task_result = db_root.create(**kwargs)
+        print("task_result", task_result.started)
+        return CreateOpenshaRuptureGenResult(task_result=task_result)
 
 
 class ToshUploadMutation(graphene.Mutation):
@@ -68,20 +85,20 @@ class ToshUploadMutation(graphene.Mutation):
         return ToshUploadMutation(ok=True)
 
 class Query(graphene.ObjectType):
-    task_results = relay.ConnectionField(
-        TaskResultConnection, description="The TaskResults used by the faction."
+    rupture_generator_results = relay.ConnectionField(
+        OpenshaRuptureGenResultConnection, description="The OpenshaRuptureGenResults."
     )
-    task_result = graphene.Field(TaskResult)
+    #     opensha_rupture_get_results = graphene.Field(OpenshaRuptureGenResult)
     node = relay.Node.Field()
 
-    def resolve_task_result(root, info):
+    def resolve_rupture_get_result(root, info):
         return db_root.get_one()
 
-    def resolve_task_results(root, info):
+    def resolve_rupture_generator_results(root, info):
         return db_root.get_all()
 
 class Mutation(graphene.ObjectType):
-    create_task_result = CreateTaskResult.Field()
+    create_task_result = CreateOpenshaRuptureGenResult.Field()
     my_upload= ToshUploadMutation.Field()
 
 db_root = TaskResultData()
