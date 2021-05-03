b4167e4e5b88f297d0ed85a66a3775f982416998
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-05-03 14:34:39 +1200

    added cors support

--- graphql_api/api.py
+++ graphql_api/api.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from graphql_api.schema import root_schema
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
     'graphql',
