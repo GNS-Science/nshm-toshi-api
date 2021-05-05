ef9539e80710ecae32e5a9cd143851cc2e63254f
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-05-06 08:35:38 +1200

    initial changes for testing

--- graphql_api/schema/custom/__init__.py
+++ graphql_api/schema/custom/__init__.py
@@ -2,6 +2,6 @@ from .strong_motion_station import StrongMotionStation
 # from .sms_file_link import SmsFileLink, SmsFileLinkConnection,\
 #  CreateSmsFileLink, SmsFileType
 from .strong_motion_station_file import SmsFile
-from .rupture_generation import RuptureGenerationTask
+from .rupture_generation import RuptureGenNewTask
 from .general_task import GeneralTask
 from .grand_inversion import GrandInversionTask
