c1e504af92a61b7b1de0dc4e92387716e93a922d
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-02-05 11:11:17 +1300

    instrument inversion_solution

--- graphql_api/schema/custom/inversion_solution.py
+++ graphql_api/schema/custom/inversion_solution.py
@@ -6,7 +6,7 @@ This module contains the schema definition for an InversionSolution.
 import graphene
 import copy
 import uuid
-import datetime as dt
+import datetime
 
 from graphene import relay
 from graphql_relay import from_global_id
@@ -20,11 +20,18 @@ from importlib import import_module
 
 from graphql_api.data_s3 import get_data_manager
 
+from datetime import datetime as dt
+from graphql_api.config import STACK_NAME, CW_METRICS_RESOLUTION
+from graphql_api.cloudwatch import ServerlessMetricWriter
+
+db_metrics = ServerlessMetricWriter(lambda_name=STACK_NAME, metric_name="MethodDuration", resolution=CW_METRICS_RESOLUTION)
+
 def resolve_node(root, info, id_field, dm_type):
     """
-    Optimisation function, looks are the query and avoids a fetch if
+    Optimisation function, looks at the query and avoids a fetch if
     we only want to resolve the id field.
     """
+    t0 = dt.utcnow()
     assert dm_type in ["table", "thing"]
 
     node_id = getattr(root, id_field)
@@ -38,9 +45,12 @@ def resolve_node(root, info, id_field, dm_type):
 
         #create an instance with just it's id attribute set
         clazz = getattr(import_module('graphql_api.schema'), type)
-        return clazz(id=nid)
+        res =  clazz(id=nid)
     else:
-        return getattr(get_data_manager(), dm_type).get_one(nid)
+        res = getattr(get_data_manager(), dm_type).get_one(nid)
+
+    db_metrics.put_duration(__name__, 'resolve_node' , dt.utcnow()-t0)
+    return res
 
 
 class LabelledTableRelation(graphene.ObjectType):
@@ -112,17 +122,19 @@ class InversionSolution(graphene.ObjectType):
         return resolve_node(root, info, 'produced_by_id', 'thing')
 
     def resolve_tables(root, info, **args):
+        t0 = dt.utcnow()
         if root.tables:
             for table in root.tables:
                 yield LabelledTableRelation(**table)
+        db_metrics.put_duration(__name__, 'InversionSolution.resolve_node' , dt.utcnow()-t0)
 
-    @staticmethod
-    def from_json(jsondata):
-        #Field type transforms...
-        created = jsondata.get('created')
-        if created:
-            jsondata['created'] = dt.datetime.fromisoformat(started)
-        return RuptureGenerationTask(**jsondata)
+    # @staticmethod
+    # def from_json(jsondata):
+    #     #Field type transforms...
+    #     created = jsondata.get('created')
+    #     if created:
+    #         jsondata['created'] = dt.fromisoformat(started)
+    #     return RuptureGenerationTask(**jsondata)
 
 class CreateInversionSolution(relay.ClientIDMutation):
     """
@@ -150,7 +162,9 @@ class CreateInversionSolution(relay.ClientIDMutation):
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **kwargs):
+        t0 = dt.utcnow()
         inversion_solution = get_data_manager().file.create('InversionSolution', **kwargs)
+        db_metrics.put_duration(__name__, 'CreateInversionSolution.mutate_and_get_payload' , dt.utcnow()-t0)
         return CreateInversionSolution(inversion_solution=inversion_solution, ok=True)
 
 
@@ -167,7 +181,7 @@ class AppendInversionSolutionTables(relay.ClientIDMutation):
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **kwargs):
-
+        t0 = dt.utcnow()
         type, nid = from_global_id(kwargs.get('id'))
         inversion_solution = get_data_manager().file.get_one_raw(nid)
 
@@ -183,9 +197,10 @@ class AppendInversionSolutionTables(relay.ClientIDMutation):
         for table in kwargs['tables']:
             table_relation = copy.copy(table)
             table_relation['identity'] = str(uuid.uuid4())
-            table_relation['created'] = dt.datetime.now(dt.timezone.utc).isoformat()
+            table_relation['created'] = dt.now(datetime.timezone.utc).isoformat()
             inversion_solution['tables'].append(table_relation)
 
         inversion_solution = get_data_manager().file.update(nid, inversion_solution)
         print('inversion_solution', inversion_solution)
+        db_metrics.put_duration(__name__, 'AppendInversionSolutionTables.mutate_and_get_payload' , dt.utcnow()-t0)
         return AppendInversionSolutionTables(inversion_solution, ok=True)
