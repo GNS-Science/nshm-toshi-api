863611331f87fc27c6bd49382b106b4d34ffccfb
Chris B Chamberlain <chrisbc@artisan.co.nz>
2022-02-04 19:06:02 +1300

    Feature/96 cloudwatch instrumentation (#98)
    * make default OFFLINE = FALSE

--- graphql_api/config.py
+++ graphql_api/config.py
@@ -3,7 +3,8 @@ This module exports comfiguration for the current system
 """
 import os
 
-def boolean_env(environ_name, default='TRUE'):
+
+def boolean_env(environ_name, default='FALSE'):
     return bool(os.getenv(environ_name, default).upper() in ["1", "Y", "YES", "TRUE"])
 
 IS_OFFLINE = boolean_env('SLS_OFFLINE') #set by serverless-wsgi plugin
