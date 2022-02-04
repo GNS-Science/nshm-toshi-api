43d0f33832cb54828b5da9bfacc243a44e7d8540
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-02-04 16:16:04 +1300

    added cloudwatch metrics;

--- graphql_api/config.py
+++ graphql_api/config.py
@@ -3,7 +3,6 @@ This module exports comfiguration for the current system
 """
 import os
 
-
 def boolean_env(environ_name, default='FALSE'):
     return bool(os.getenv(environ_name, default).upper() in ["1", "Y", "YES", "TRUE"])
 
@@ -13,7 +12,6 @@ if IS_OFFLINE:
     ES_ENDPOINT = "http://localhost:9200"
 else:
     ES_ENDPOINT = os.getenv("ES_ENDPOINT", '')
-
 
 ES_INDEX = os.getenv("ES_INDEX", "toshi-index")
 ES_REGION = os.getenv("ES_REGION", 'us-east-1')
