632e0673a39df61cdcd226dfd210bb09a0612e92
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-02-24 22:54:55 +1300

    add build_one; remove build_child/parent; get_one caller has no clazz; from_json builds a TaskTaskRelation;

--- graphql_api/data/thing_relation_data.py
+++ graphql_api/data/thing_relation_data.py
@@ -25,53 +25,65 @@ class ThingRelationData(BaseData):
         self._db_manager.thing.add_child_relation(thing_id=parent_id, relation_id=child_id, relation_clazz=child_clazz)
         self._db_manager.thing.add_parent_relation(thing_id=child_id, relation_id=parent_id, relation_clazz=parent_clazz)
 
-    def get_one(self, _id, clazz):
+    def get_one(self, _id):
         """
         Args:
             _id (string): the object id
         Returns:
             File: the Thing object
         """
-        print('THING RELATION GET ONE', _id, clazz)
-        jsondata = self._read_object(_id, clazz)
+        print('THING RELATION GET ONE', _id)
+        jsondata = self._read_object(_id)
         logger.info("get_one: %s" % str(jsondata))
         relation =  self.from_json(jsondata)
         return relation
 
-    def build_parent(self, parent_id, clazz_name):
+    def build_one(self, parent_id, child_id):
         """
         Args:
-            parent_id (string): the parent id
-            child_id (string): the child id
+            parent_id (string): the object id
+            child_id(string): the object id
         Returns:
-            File: the Thing object
+            File: the TaskTaskRelation object
         """
-
-        jsondata = {'id': parent_id, 'clazz_name': clazz_name}
-        logger.info("build_one: %s" % str(jsondata))
-        relation =  self.from_json(jsondata)
-        return relation
-    
-    def build_child(self, child_id, clazz_name):
-        """
-        Args:
-            parent_id (string): the parent id
-            child_id (string): the child id
-        Returns:
-            File: the Thing object
-        """
-
-        jsondata = {'id': child_id, 'clazz_name': clazz_name}
+        jsondata = {'parent_id': parent_id, 'child_id': child_id}
         logger.info("build_one: %s" % str(jsondata))
         relation =  self.from_json(jsondata)
         return relation
 
-    def _read_object(self, object_id, object_type):
-        key = f'ThingData/{object_id}'
-        print ('thing relation reads', key, object_type)
-        obj = ToshiThingObject.get(key, 'ThingData')
-        print(obj)
-        return obj.object_content
+    # def build_parent(self, parent_id, clazz_name):
+    #     """
+    #     Args:
+    #         parent_id (string): the parent id
+    #         child_id (string): the child id
+    #     Returns:
+    #         File: the Thing object
+    #     """
+
+    #     jsondata = {'id': parent_id, 'clazz_name': clazz_name}
+    #     logger.info("build_one: %s" % str(jsondata))
+    #     relation =  self.from_json(jsondata)
+    #     return relation
+
+    # def build_child(self, child_id, clazz_name):
+    #     """
+    #     Args:
+    #         parent_id (string): the parent id
+    #         child_id (string): the child id
+    #     Returns:
+    #         File: the Thing object
+    #     """
+    #     jsondata = {'id': child_id, 'clazz_name': clazz_name}
+    #     logger.info("build_one: %s" % str(jsondata))
+    #     relation =  self.from_json(jsondata)
+    #     return relation
+
+    # def _read_object(self, object_id):
+    #     key = f'ThingData/{object_id}'
+    #     print ('thing relation reads', key)
+    #     obj = ToshiThingObject.get(key, 'ThingData')
+    #     #print (obj)
+    #     return obj.object_content
 
     @staticmethod
     def from_json(jsondata):
@@ -81,6 +93,9 @@ class ThingRelationData(BaseData):
         #     jsondata['created'] = dt.datetime.fromisoformat(created)
 
         # jsondata.pop('id', None)
-        clazz_name = jsondata.pop('clazz_name', None)
-        clazz = getattr(import_module('graphql_api.schema'), clazz_name)
+        clazz = getattr(import_module('graphql_api.schema'), "TaskTaskRelation")
+
+        #id is no longer a class attribute, but some old objects may still exist
+        jsondata.pop('id', None)
+        jsondata.pop('clazz_name', None)
         return clazz(**jsondata)
