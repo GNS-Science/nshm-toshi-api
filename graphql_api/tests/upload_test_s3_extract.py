faaef0635c01388492747e70663dd3660152c77d
benjamineac <ben.albrecht.chamberlain@gmail.com>
2022-03-01 11:42:51 +1300

    add upload func to test s3

--- graphql_api/tests/upload_test_s3_extract.py
+++ graphql_api/tests/upload_test_s3_extract.py
@@ -1,6 +1,9 @@
 import boto3
 import os
-# from graphql_api.schema import SearchManager
+import json
+
+from graphql_api.schema import SearchManager
+from graphql_api.config import REGION, ES_ENDPOINT, ES_INDEX, S3_BUCKET_NAME
 # raises ModuleNotFoundError: No module named 'graphql_api'
 
 '''
@@ -10,21 +13,20 @@ sls dynamodb start --stage local & sls s3 start & SLS_OFFLINE=1 TOSHI_FIX_RANDOM
 docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.8.0
 S3_TEST_DATA_PATH={path/to/your/data} python3 graphql_api/tests/upload_test_s3_extract.py 
 '''
-
+bucket_name = S3_BUCKET_NAME
 local_path = os.environ.get('S3_TEST_DATA_PATH', None)
 client = boto3.client('s3', 
     aws_access_key_id='S3RVER',
     aws_secret_access_key='S3RVER',
-    endpoint_url='http://localhost:4569/')
-bucket_name = os.environ.get('S3_BUCKET_NAME', "nshm22-toshi-api-local")
+    endpoint_url='http://localhost:4569/',
+    region_name=REGION)
 s3 = boto3.resource('s3')
-search_manager = sm.SearchManager(endpoint=ES_ENDPOINT, es_index=ES_INDEX, awsauth=None)
+search_manager = SearchManager(endpoint=ES_ENDPOINT, es_index=ES_INDEX, awsauth=None)
 try:
     client.create_bucket(Bucket=bucket_name)
 except:
     print(f'Bucket {bucket_name} Exists!')
 bucket = s3.Bucket(bucket_name, client=client)
-
 
 def upload_objects(root_path):
     try:
@@ -33,9 +35,15 @@ def upload_objects(root_path):
             for file in files:
                 key = "%s/%s" % (directory_name, file)
                 print(f'Uploading {key}!')
-                bucket.upload_file(os.path.join(path, file), key)
-                # es_key = key.replace("/", "_")
-                # search_manager.index_document(es_key, body)
+                filepath = os.path.join(path, file)
+                bucket.upload_file(filepath, key)
+                if 'object.json' in key:
+                    es_key = key.replace("/", "_")
+                    with open(filepath, 'r') as myfile:
+                        data=myfile.read()
+                        print(data)
+                        search_manager.index_document(es_key, data)
+                        assert 0
     except Exception as err:
         print(err)
     
