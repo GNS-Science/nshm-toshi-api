a4d326bd365cacd489154b95aa90ec6b137a51fb
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-11-10 11:15:49 +1300

    improve cli for OHS analysis

--- scripts/elastic_cli.py
+++ scripts/elastic_cli.py
@@ -2,6 +2,7 @@ import os
 import click
 
 from graphql_api.config import ES_ENDPOINT
+from graphql_relay import to_global_id
 from elasticsearch_dsl import Search
 from elasticsearch_dsl import connections, Q
 
@@ -12,20 +13,35 @@ def slt():
     pass
 
 @slt.command(name='ohs')
-@click.option('-D', '--disaggs', is_flag=True)
-@click.option('-v', '--verbose', is_flag=True)
-def cli_OHS(disaggs, verbose):
+@click.option('-f', '--filter',
+    type=click.Choice(['DAG', 'HAZ'], case_sensitive=False),
+    help='list filter solutions by subtype.')
+@click.option('-l', '--list-ids', is_flag=True, default=False, help="list the toshi_ids, with no aggregation (verbose option).")
+@click.option('-v', '--verbose', is_flag=True, default=False, help='print extra information.')
+def cli_OHS(filter, list_ids, verbose):
     """Get OpenquakeHazardSolution hits - either hazard or disagg."""
     s = Search()
-    q1 = Q("term", meta__k="hazard_agg_target") # this marks a disagg
-    q2 = Q("term", clazz_name__keyword="OpenquakeHazardSolution")
-    if disaggs:
-        click.echo('GET disagggregation solutions')
-    else:
-        click.echo('GET hazard solutions')
-        q1 = ~q1 # invert for hazard / non-disaggs
-    s = s.query( q2 & q1)
-    # aggregate by month ...
+    qFilter = Q("term", meta__k="hazard_agg_target") # this marks a disagg
+    qClass = Q("term", clazz_name__keyword="OpenquakeHazardSolution")
+    if filter and filter=='DAG':
+        if verbose: click.echo('GET disagggregation solutions')
+    elif filter and filter=='HAZ':
+        if verbose: click.echo('GET hazard solutions')
+        qFilter = ~qFilter # invert for hazard / non-disaggs
+
+    expr = qClass & qFilter if filter else qClass
+    s = s.query(expr)
+
+    # the shortform, just dump the ids to stdout
+    if list_ids:
+        for hit in s.scan():
+            if verbose:
+                click.echo(f'{to_global_id(hit.clazz_name, hit.id)}, {hit.clazz_name}, {hit.id}')
+            else:
+                click.echo(to_global_id(hit.clazz_name, hit.id))
+        return
+
+    # the longform, aggregate by month ...
     s.aggs.bucket('solutions_per_month', 'date_histogram', field='created', interval='month')
     s = s.extra(explain=True, track_total_hits=True)
 
