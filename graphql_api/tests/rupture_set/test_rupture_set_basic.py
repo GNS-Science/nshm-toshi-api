1d984bee4e0a18131f6adb8146a861ce3597a65c
Chris B Chamberlain <chrisbc@artisan.co.nz>
2025-12-08 17:28:50 +1300

    added round trip tests for rupture_set

--- graphql_api/tests/rupture_set/test_rupture_set_basic.py
+++ graphql_api/tests/rupture_set/test_rupture_set_basic.py
@@ -44,3 +44,82 @@ def test_create_rupture_set_happy_case(graphql_client, rupture_generation_task, 
     rupture_set = executed['data']['create_rupture_set']['rupture_set']
     assert from_global_id(rupture_set['id']) == ("RuptureSet", "100000")
     assert rupture_set["produced_by"]['__typename'] == "RuptureGenerationTask"
+
+
+@pytest.fixture
+def create_rupture_set(graphql_client, rupture_generation_task, create_rupture_set_mutation):
+
+    # prepare the generation task
+    print(rupture_generation_task)
+
+    # Now create the Rupture Generation task
+    executed = graphql_client.execute(
+        create_rupture_set_mutation,
+        variable_values=dict(
+            created=datetime.datetime.now(datetime.UTC),
+            md5_digest="digest",
+            file_name="file_name",
+            file_size=1000,
+            produced_by=rupture_generation_task['id'],
+            metrics=[{"k": "some_shelley_creation", "v": "20"}],
+        ),
+    )
+    yield executed['data']['create_rupture_set']['rupture_set']
+
+
+@mock_aws()
+def test_rupture_set_round_trip_happy_case(graphql_client, create_rupture_set):
+
+    VERIFY_RUPT_SET = '''
+        query get_node($node_id: ID!){
+          node(id: $node_id) {
+            __typename
+            ... on RuptureSet {
+                id
+                created
+                file_name
+                md5_digest
+                file_size
+                produced_by { id, __typename }
+                arguments { k v}
+                metrics { k v}
+            }
+          }
+        }
+        '''
+    # query the API for our fresh rupture set using `RuptureSet` ...
+    result = graphql_client.execute(VERIFY_RUPT_SET, variable_values=dict(node_id=create_rupture_set['id']))
+    print(result)
+
+    node = result['data']['node']
+    assert from_global_id(node['id'])[0] == "RuptureSet"
+    assert node["__typename"] == "RuptureSet"
+    assert node["produced_by"]['__typename'] == "RuptureGenerationTask"
+    assert node["arguments"] is None
+    assert node["metrics"][0]['k'] == "some_shelley_creation"
+
+
+@mock_aws()
+def test_rupture_set_round_trip_happy_case_as_file_interface(graphql_client, create_rupture_set):
+
+    VERIFY_FILE_INTERFACE = '''
+        query get_node($node_id: ID!){
+          node(id: $node_id) {
+            __typename
+            ... on Node { id }
+            ... on FileInterface {
+                file_name
+                md5_digest
+                file_size             
+            }
+          }
+        }
+        '''
+    # query the API for our fresh rupture set using (`Node`, `FileInterface`) ...
+    result = graphql_client.execute(VERIFY_FILE_INTERFACE, variable_values=dict(node_id=create_rupture_set['id']))
+    print(result)
+
+    node = result['data']['node']
+    assert node["__typename"] == "RuptureSet"
+    assert from_global_id(node['id'])[0] == "RuptureSet"
+    assert node['file_name'] == "file_name"
